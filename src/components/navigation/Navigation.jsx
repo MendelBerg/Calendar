f1e0feab5912a7911a52f9828e95e7db656db69a
Alex <mendelberg44@gmail.com>
2021-06-16 12:56:27 +0300

    navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import СlassNames from 'classnames';
 
 import { days } from '../../utils/dateUtils.js';
+import './navigation.scss';
 
 const Navigation = ({ weekDates }) => {
+  const currentDate = new Date().getDate();
+
   return (
     <header className="calendar__header">
       {weekDates.map((dayDate, index) => (
-        <div key={index} className="calendar__day-label day-label">
+        <div
+          key={index}
+          className={СlassNames('calendar__day-label day-label', {
+            'day-label_current': currentDate === dayDate.getDate(),
+          })}
+        >
           <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
           <span className="day-label__day-number">{dayDate.getDate()}</span>
         </div>
