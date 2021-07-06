18ddf09c4b256094e81aa47890e765d204c5aad4
Alexander <mendelberg44@gmail.com>
2021-07-06 08:54:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,54 +9,54 @@ import { fetchEvents } from './gateway/events.js';
 import './common.scss';
 
 const App = () => {
-  const [weekStartDate, setWeekStartDate] = useState(new Date());
-  const [isVisibleModal, setIsVisibleModal] = useState(false);
-  const [events, setEvents] = useState([]);
+	const [weekStartDate, setWeekStartDate] = useState(new Date());
+	const [isVisibleModal, setIsVisibleModal] = useState(false);
+	const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    fetchEvents().then(response => {
-      setEvents(
-        response.map(el => ({
-          ...el,
-          dateFrom: new Date(el.dateFrom),
-          dateTo: new Date(el.dateTo),
-        })),
-      );
-    });
-  }, []);
+	useEffect(() => {
+		fetchEvents().then(response => {
+			setEvents(
+				response.map(el => ({
+					...el,
+					dateFrom: new Date(el.dateFrom),
+					dateTo: new Date(el.dateTo),
+				})),
+			);
+		});
+	}, []);
 
-  function switchWeek(nextWeek = true) {
-    const dateCopy = new Date(weekStartDate);
-    dateCopy.setDate(dateCopy.getDate() + (nextWeek ? 7 : -7));
-    setWeekStartDate(dateCopy);
-  }
+	function switchWeek(nextWeek = true) {
+		const dateCopy = new Date(weekStartDate);
+		dateCopy.setDate(dateCopy.getDate() + (nextWeek ? 7 : -7));
+		setWeekStartDate(dateCopy);
+	}
 
-  function returnToday() {
-    setWeekStartDate(new Date());
-  }
+	function returnToday() {
+		setWeekStartDate(new Date());
+	}
 
-  function showModal() {
-    setIsVisibleModal(true);
-  }
+	function showModal() {
+		setIsVisibleModal(true);
+	}
 
-  function hideModal() {
-    setIsVisibleModal(false);
-  }
+	function hideModal() {
+		setIsVisibleModal(false);
+	}
 
-  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+	const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
-  return (
-    <>
-      {!isVisibleModal ? null : <Modal onCloseModal={hideModal} />}
-      <Header
-        weekDates={weekDates}
-        onSwitchWeek={switchWeek}
-        onReturnToday={returnToday}
-        onCreateEvent={showModal}
-      />
-      <Calendar weekDates={weekDates} events={events} />
-    </>
-  );
+	return (
+		<>
+			{!isVisibleModal ? null : <Modal onCloseModal={hideModal} />}
+			<Header
+				weekDates={weekDates}
+				onSwitchWeek={switchWeek}
+				onReturnToday={returnToday}
+				onCreateEvent={showModal}
+			/>
+			<Calendar weekDates={weekDates} events={events} />
+		</>
+	);
 };
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,27 +5,29 @@ import { months } from '../../utils/dateUtils.js';
 import './header.scss';
 
 const Header = ({ weekDates, onSwitchWeek, onReturnToday, onCreateEvent }) => {
-  const [month, nextMonth] = [...new Set(weekDates.map(day => day.getMonth()))];
+	const [month, nextMonth] = [...new Set(weekDates.map(day => day.getMonth()))];
 
-  return (
-    <header className="header">
-      <button className="button create-event-btn" onClick={onCreateEvent}>
-        <i className="fas fa-plus create-event-btn__icon"></i>Create
-      </button>
-      <div className="navigation">
-        <button className="navigation__today-btn button" onClick={onReturnToday}>Today</button>
-        <button className="icon-button navigation__nav-icon" onClick={() => onSwitchWeek(false)}>
-          <i className="fas fa-chevron-left"></i>
-        </button>
-        <button className="icon-button navigation__nav-icon" onClick={onSwitchWeek}>
-          <i className="fas fa-chevron-right"></i>
-        </button>
-        <span className="navigation__displayed-month">{
-          !nextMonth ? months[month] : `${months[month]} - ${months[nextMonth]}`
-        }</span>
-      </div>
-    </header>
-  );
+	return (
+		<header className="header">
+			<button className="button create-event-btn" onClick={onCreateEvent}>
+				<i className="fas fa-plus create-event-btn__icon"></i>Create
+			</button>
+			<div className="navigation">
+				<button className="navigation__today-btn button" onClick={onReturnToday}>
+					Today
+				</button>
+				<button className="icon-button navigation__nav-icon" onClick={() => onSwitchWeek(false)}>
+					<i className="fas fa-chevron-left"></i>
+				</button>
+				<button className="icon-button navigation__nav-icon" onClick={onSwitchWeek}>
+					<i className="fas fa-chevron-right"></i>
+				</button>
+				<span className="navigation__displayed-month">
+					{!nextMonth ? months[month] : `${months[month]} - ${months[nextMonth]}`}
+				</span>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
